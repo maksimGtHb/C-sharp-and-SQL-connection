256204b1840d3317cfc6dbb864f804afb0af61fe
maksc <maksc@DESKTOP-4CFN4N0>
2024-06-15 19:06:50 +0300

    Add project files.

---
+++ New/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace New
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(63, 121);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(870, 386);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(340, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Список сотрудников";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(322, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(369, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Сортировать при нажатии на столбец";
+            this.label2.SizeChanged += new System.EventHandler(this.label2_SizeChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(151, 525);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(887, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Чтобы проверить сортировку, можно вписать в таблицу данные на русском языке и наж" +
+    "ать на столбец";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(987, 579);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView3);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ New/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace New
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           //запуск базы данных и ее запросов
+           LoadData();
+        }
+
+        private void LoadData()
+        {
+          //строка подключения
+          string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\maksc\source\repos\New\New\Database1.mdf;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+               
+                //SQL запрос на отображение данных
+                string query = @"
+                    SELECT persons.first_name, persons.second_name, persons.last_name, 
+                           status.name AS status_name, 
+                           deps.name AS deps_name, 
+                           posts.name AS posts_name, 
+                           persons.date_employ, 
+                           persons.date_uneploy
+                    FROM persons 
+                    INNER JOIN status ON persons.id=status.id 
+                    INNER JOIN deps ON persons.id=deps.id 
+                    INNER JOIN posts ON persons.id=posts.id;";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+             //подключение табличного поля к SQL
+
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                dataGridView3.DataSource = dataTable;
+            }
+
+            // Цикл по сортировки столбцов
+            foreach (DataGridViewColumn column in dataGridView3.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.Automatic;
+            }
+
+            // Обработчик события для сортировки
+            dataGridView3.ColumnHeaderMouseClick += new DataGridViewCellMouseEventHandler(dataGridView3_ColumnHeaderMouseClick);
+        }
+        //создание метода, при нажатии мыши на столбец
+        private void dataGridView3_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DataGridViewColumn newColumn = dataGridView3.Columns[e.ColumnIndex];
+            DataGridViewColumn oldColumn = dataGridView3.SortedColumn;
+            ListSortDirection direction;
+
+            // Если столбец не отсортирован, сортировка по возрастанию
+            if (oldColumn != null)
+            {
+                if (oldColumn == newColumn && dataGridView3.SortOrder == System.Windows.Forms.SortOrder.Ascending)
+                {
+                    direction = ListSortDirection.Ascending;
+                }
+                else
+                {
+                    direction = ListSortDirection.Descending;
+                }
+            }
+            else
+            {
+                direction = ListSortDirection.Ascending;
+            }
+
+            
+
+            // Выполнение метода
+            dataGridView3.Sort(newColumn, direction);
+        }
+
+        private void label2_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+/* private void LoadData ()
+ {
+     try
+     {
+         sqlAdapter = new SqlDataAdapter("SELECT first_name, second_name, last_name, status.name," +
+             " deps.name, posts.name, date_employ, date_uneploy  " +
+             "FROM persons " +
+             "INNER JOIN status on persons.id=status.id " +
+             "INNER JOIN deps on persons.id=deps.id " +
+             "INNER JOIN posts on persons.id=posts.id;", con); 
+
+         //", persons, posts, status", con);
+         sqlBuilder = new SqlCommandBuilder(sqlAdapter);
+
+         dataSet = new DataSet();
+         DataTable dataTable = new DataTable();
+         sqlAdapter.Fill(dataTable);
+         dataGridView2.DataSource = dataTable;
+
+         dataGridView2.DataSource = dataSet.Tables["persons"];
+
+     }
+
+     catch (Exception ex)
+     { 
+         MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+     }
+ }
+
+ private void Form1_Load(object sender, EventArgs e)
+ {
+     con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\maksc\source\repos\New\New\Database1.mdf;Integrated Security=True");
+     con.Open();
+
+ }
+}
+}
+*/
